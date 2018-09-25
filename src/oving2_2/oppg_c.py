3a0529ca767a47a977b01534f9d593b63baee8d0
Magnus Conrad Hyll <magnus@hyll.no>
2018-09-25 13:46:46 +0200

    Øving 2.2 og 3.1

--- src/oving2_2/oppg_c.py
+++ src/oving2_2/oppg_c.py
@@ -1,4 +1,3 @@
-import numpy as np
 import tensorflow as tf
 from oving2_2.coding import create_coding, encode_string, encode_feature
 
@@ -7,14 +6,13 @@ class LongShortTermMemoryModel:
     def __init__(self, encodings_size_in, encodings_size_out):
         # Model constants
         cell_state_size = 128
-        cell_state_size = encodings_size_in
 
         # Cells
         cell = tf.contrib.rnn.BasicLSTMCell(cell_state_size)
 
         # Model input
         self.batch_size = tf.placeholder(tf.int32, [], name='batch_size')  # Needed by cell.zero_state call, and can be dependent on usage (training or generation)
-        self.x = tf.placeholder(tf.float32, [3, None, encodings_size_in], name='x')  # Shape: [batch_size, max_time, encodings_size]
+        self.x = tf.placeholder(tf.float32, [None, None, encodings_size_in], name='x')  # Shape: [batch_size, max_time, encodings_size]
         self.y = tf.placeholder(tf.float32, [None, encodings_size_out], name='y')  # Shape: [batch_size, encodings_size]
         self.in_state = cell.zero_state(self.batch_size, tf.float32)  # Can be used as either an input or a way to get the zero state
 
@@ -35,8 +33,8 @@ class LongShortTermMemoryModel:
         self.loss = tf.losses.softmax_cross_entropy(self.y, logits)
 
 
-def train(x_train, y_train, char_encodings, index_to_char, batch_size):
-    model = LongShortTermMemoryModel(len(index_to_char), len(y_train))
+def train(x_train, y_train, x_encoding_len, y_encoding_len, out_labels, word_to_test):
+    model = LongShortTermMemoryModel(x_encoding_len, y_encoding_len)
 
     # Training: adjust the model so that its loss is minimized
     minimize_operation = tf.train.RMSPropOptimizer(0.05).minimize(model.loss)
@@ -48,54 +46,47 @@ def train(x_train, y_train, char_encodings, index_to_char, batch_size):
     session.run(tf.global_variables_initializer())
 
     # Initialize model.in_state
-    zero_state = session.run(model.in_state, {model.batch_size: batch_size})
+    zero_state = session.run(model.in_state, {model.batch_size: len(x_train)})
 
     for epoch in range(500):
-        session.run(minimize_operation, {model.batch_size: batch_size, model.x: x_train, model.y: y_train, model.in_state: zero_state})
+        session.run(minimize_operation, {model.batch_size: 1, model.x: x_train, model.y: y_train, model.in_state: zero_state})
 
         if epoch % 10 == 9:
             print("epoch", epoch)
-            print("loss", session.run(model.loss, {model.batch_size: batch_size, model.x: x_train, model.y: y_train, model.in_state: zero_state}))
+            print("loss", session.run(model.loss, {model.batch_size: 1, model.x: x_train, model.y: y_train, model.in_state: zero_state}))
 
-            # Generate characters from the initial characters ' h'
-            state = session.run(model.in_state, {model.batch_size: batch_size})
-            y, state = session.run([model.f, model.out_state], {model.batch_size: batch_size, model.x: [[char_encodings['c']]], model.in_state: state})  # ' '
-            y, state = session.run([model.f, model.out_state], {model.batch_size: batch_size, model.x: [[char_encodings['a']]], model.in_state: state})  # 'h'
-            y, state = session.run([model.f, model.out_state], {model.batch_size: batch_size, model.x: [[char_encodings['t']]], model.in_state: state})  # 'h'
-            y, state = session.run([model.f, model.out_state], {model.batch_size: batch_size, model.x: [[char_encodings[' ']]], model.in_state: state})  # 'h'
-            text = index_to_char[y.argmax()]
-            # for c in range(50):
-            #     y, state = session.run([model.f, model.out_state], {
-            #         model.batch_size: batch_size,
-            #         model.x: [[char_encodings[index_to_char[y.argmax()]]]],
-            #         model.in_state: state
-            #     })
-            #     text += index_to_char[y[0].argmax()]
-            print(text)
+            # Generate emoji from word_to_test
+            state = session.run(model.in_state, {model.batch_size: 1})
+            y, state = session.run([model.f, model.out_state], {
+                model.batch_size: 1,
+                model.x: [word_to_test],
+                model.in_state: state
+            })
+
+            print(out_labels[y.argmax()])
 
     session.close()
 
 
 if __name__ == '__main__':
-    alphabet = ' hatcm'  # Strings are iterable, no need for the list index_to_chars
-    char_encodings, _ = create_coding(alphabet)
-
-    emojis = [
-        '\U0001f3a9',
-        '\U0001f408',
-        '\U0001f468'
+    train_data = [
+        ('hat ', '\U0001f3a9'),
+        ('cat ', '\U0001f408'),
+        ('matt', '\U0001f468'),
+        ('flat', '\U0001f3d8'),
+        ('cap ', '\U0001f9e2'),
+        ('cup ', '\U0001f964'),
+        ('dog ', '\U0001f415'),
+        ('many', '\U0000274c')
     ]
+
+    alphabet = ''.join(set(''.join([data[0] for data in train_data])))
+    emojis = ''.join(set([data[1] for data in train_data]))
+
+    char_encodings, _ = create_coding(alphabet)
     emoji_encodings, _ = create_coding(emojis)
 
-    x_train = [
-        encode_string('hat ', char_encodings),
-        encode_string('cat ', char_encodings),
-        encode_string('matt', char_encodings)
-    ]
-    y_train = [
-        encode_feature('\U0001f3a9', emoji_encodings),
-        encode_feature('\U0001f408', emoji_encodings),
-        encode_feature('\U0001f468', emoji_encodings)
-    ]
+    x_train = [encode_string(data[0], char_encodings) for data in train_data]
+    y_train = [encode_feature(data[1], emoji_encodings) for data in train_data]
 
-    train(x_train, y_train, char_encodings, alphabet, batch_size=3)
+    train(x_train, y_train, len(alphabet), len(emojis), emojis, encode_string('many cat', char_encodings))
